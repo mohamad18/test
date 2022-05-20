d46fb10de8ae36735f2952331301907b7d062d80
mohamad18 <44187262+mohamad18@users.noreply.github.com>
2022-05-20 15:30:50 +0700

    initial commit

---
+++ Actions.kt
@@ -0,0 +1,6 @@
+package com.trace.login_google
+
+enum class Actions {
+    START,
+    STOP
+}
---
+++ Utils.kt
@@ -0,0 +1,7 @@
+package com.trace.login_google
+
+import android.util.Log
+
+fun log(msg: String) {
+    Log.d("CORETAN-DIGITAL-SERVICE", msg)
+}
